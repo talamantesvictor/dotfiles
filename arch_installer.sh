014aa85dd5bc219ece77b7925e6cbc51db74a42c
Victor Talamantes <victor@binmatter.com>
2022-08-02 17:05:44 -0600

    add commented shell script with a list of apps

---
+++ arch_installer.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# ******************************************************
+# System and CLI apps
+# ******************************************************
+
+# pacman -Syy              # System update
+# pacman -S git            # Version control
+# pacman -S zip unzip      # Compressed files
+# pacman -S xorg-xrandr    # For my multi monitor setup
+# pacman -S xorg-xinit     # xorg startup config
+# pacman -S pamixer        # Audio mixer
+# pacman -S brightnessctl  # Brightness
+# pacman -S picom          # Compositor
+# pacman -S neovim         # Install neovim text editor
+# pacman -S nodejs npm     # Used for development and neovim plugin
+# pacman -S ripgrep        # Used by neovim telescope plugin
+
+# *****************************************************
+# GUI Applications
+# *****************************************************
+
+# pacman -S firefox        # Web browser
+# pacman -S nitrogen       # Wallpapers manager
+# pacman -S nemo           # File manager
+# pacman -S nemo-fileroller# Extract/Compress files in Nemo
+# pacman -S pavucontrol    # Audio controls
+# pacman -S mpv yt-dlp     # Video player
+# pacman -S lxappearance   # Theming
+# pacman -S rofi           # Launcher
+
+# We need yay for installing the following software
+# yay -S qimgv-git         # Image viewer
+
+# Other
+# sh -c "$(curl https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/install.sh)"
